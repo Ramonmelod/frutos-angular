9b7924cde3dfe9925582a76fc923fcb41ff143df
Ramonmelod <contato@ramonmelo.com.br>
2026-01-08 09:38:21 -0300

    feat: initial implementation of the productGridComponent

---
+++ src/app/shared/components/productGrid.js/productGrid.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { ProductGridComponent } from './productGrid.component'
+
+describe('ProductGridComponent', () => {
+    console.log('ProductGridComponent test - INIT')
+    let component: ProductGridComponent
+    let fixture: ComponentFixture<ProductGridComponent>
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [ProductGridComponent],
+        }).compileComponents()
+
+        fixture = TestBed.createComponent(ProductGridComponent)
+        component = fixture.componentInstance
+        fixture.detectChanges()
+    })
+
+    it('should create', () => {
+        console.log('ProductGridComponent - should create')
+        expect(component).toBeTruthy()
+    })
+    console.log('ProductGridComponent test - STOP')
+})
